1dffd959326f336496b3bb79907669baf5c2654f
Luke <luke.fiji@gmail.com>
2016-03-05 15:56:26 -0800

    Updated profile links

--- index.php
+++ index.php
@@ -3,12 +3,18 @@
 <div class="container">
   <div class="headline">Hello, I'm Luke Fiji</div>
     <div class="headline-sub">
-      I grew up learning how to use computers at a very young age experimenting with Microsoft Paint. In my teenage years, I discovered that Photoshop was installed on my computer and began to tinker around with it, following step-by-step tutorials online, eventually leading to my passion for graphic design. When MySpace came around, I applied my knowledge of Photoshop to customize my profile, learning a bit of HTML and CSS along the way. I soon began to appreciate the intricacies of web design and decided to pursue an informatics degree, specializing in human-computer interaction, at UC Irvine. With my bachelor's degree now in hand, I enjoy designing graphics and websites, for fun.
-  </div>
+      I grew up learning how to use computers at a very young age experimenting with Microsoft Paint. In my teenage years, I discovered that Photoshop was installed on my computer and began to tinker around with it, following step-by-step tutorials online, eventually leading to my passion for graphic design. When MySpace came around, I applied my knowledge of Photoshop to customize my profile, learning a bit of HTML and CSS along the way. I soon began to appreciate the intricacies of web design and decided to pursue an informatics degree at UC Irvine, specializing in human-computer interaction. With my bachelor's degree now in hand, I enjoy designing websites and graphics, for fun.
+    </div>
 
   <hr></hr>
 
   <div class="headline">SKILLSET</div>
+  <div class="headline-sub">
+    <a href="http://kidfiji.net/kidfiji/wp-content/uploads/2016/03/Luke-Fiji-Resume.pdf">Resume</a> /
+    <a href="http://www.linkedin.com/in/lukefiji">LinkedIn</a>
+    <a href="https://github.com/lukefiji">GitHub</a> /
+    <a href="http://codepen.io/lukefiji/">CodePen</a>
+  </div>
   <div class="row">
   <div class="four columns skills">
     <h6 class="card-skills-header">Skills</h6>
@@ -30,13 +36,12 @@
       <ul>
         <li>HTML5</li>
         <li>CSS3</li>
-        <li>SASS</li>
+        <li>Sass</li>
         <li>Bourbon/Neat</li>
         <li>Bootstrap</li>
         <li>JavaScript</li>
         <li>jQuery</li>
         <li>WordPress</li>
-        <li>PHP</li>
       </ul>
     </div>
   </div>
@@ -54,13 +59,6 @@
     </div>
   </div>
   </div>
-</div>
-
-<hr></hr>
-
-<div class="headline">LINKS</div>
-<div class="headline-sub">
-  <a href="#">Resume</a> / <a href="https://github.com/lukefiji">GitHub</a> / <a href="http://codepen.io/lukefiji/">CodePen</a>
 </div>
 
 <?php get_footer(); ?>
